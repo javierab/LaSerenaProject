58ed416bb3bae2b733889493fcab1792be7db6af
Javiera Born <javieraborn@gmail.com>
2015-08-19 17:19:59 -0300

    Python Notebook for the project

--- pandasTest.py
+++ pandasTest.py
@@ -1,7 +1,6 @@
 import pandas as pds
 import numpy as np
 from numpy import corrcoef, sum, log, arange
-from numpy.random import rand
 from pylab import pcolor, show, colorbar, xticks, yticks
 import os
 
